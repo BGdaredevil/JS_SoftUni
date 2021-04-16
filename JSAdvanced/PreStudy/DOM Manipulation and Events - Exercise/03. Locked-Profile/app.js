74e32ba870dbc445b0b65d3236d5b7fcfb51d711
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-04-16 23:21:49 +0300

    solved

--- JSAdvanced/PreStudy/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ JSAdvanced/PreStudy/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,40 @@
 function lockedProfile() {
-    console.log('TODO...')
+  let buttons = Array.from(document.querySelectorAll(".profile input")).filter(
+    (el) => el.type === "radio"
+  );
+  let showBtns = Array.from(document.querySelectorAll(".profile button"));
+  let checked = Array.from(document.querySelectorAll(".profile input")).filter(
+    (el) => el.checked === true && el.value === "lock"
+  );
+
+  checked.forEach((el) => (el.parentElement.lastElementChild.disabled = true));
+  buttons.forEach((btn) => btn.addEventListener("click", btnEv));
+  showBtns.forEach((btn) => btn.addEventListener("click", showEv));
+
+  console.log(buttons);
+  console.log(showBtns);
+
+  function showEv(e) {
+    if (e.target.innerText === "Show more") {
+      e.target.innerText = "Hide it";
+      e.target.previousElementSibling.setAttribute("style", "display: block");
+    } else {
+      e.target.innerText = "Show more";
+      e.target.previousElementSibling.setAttribute("style", "display: none");
+    }
+
+    console.log(e.target.previousElementSibling);
+  }
+
+  function btnEv(e) {
+    if (e.target.value === "lock") {
+      //console.log(e.target.value);
+      e.target.parentElement.lastElementChild.disabled = true;
+      //console.log(e.target.parentElement.lastElementChild.disabled);
+    } else {
+      //console.log(e.target.value);
+      e.target.parentElement.lastElementChild.disabled = false;
+      //console.log(e.target.parentElement.lastElementChild.disabled);
+    }
+  }
 }
--- JSAdvanced/PreStudy/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ JSAdvanced/PreStudy/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+  let menu = document.getElementById("menu");
+  let text = document.getElementById("newItemText");
+  let val = document.getElementById("newItemValue");
+  let newest = document.createElement("option");
+
+  newest.textContent = text.value;
+  text.value = "";
+  newest.value = val.value;
+  val.value = "";
+
+  menu.appendChild(newest);
 }
