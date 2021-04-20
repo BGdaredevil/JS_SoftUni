e4bdfd21c9c5bd3429dc409f81882924aae8d716
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-04-20 21:24:01 +0300

    Solveds - do not add event listeners

--- JSAdvanced/PreStudy/Advanced Functions - Exercise/07. Simple Calculator/app.js
+++ JSAdvanced/PreStudy/Advanced Functions - Exercise/07. Simple Calculator/app.js
@@ -1,7 +1,22 @@
 function calculator() {
-    // TODO:
+  let numOne;
+  let numTwo;
+  let result;
+  let operations = {
+    init(num1, num2, res) {
+      numOne = document.querySelector(num1);
+      numTwo = document.querySelector(num2);
+      result = document.querySelector(res);
+    },
+    add() {
+      result.value = Number(numOne.value) + Number(numTwo.value);
+    },
+    subtract() {
+      result.value = Number(numOne.value) - Number(numTwo.value);
+    },
+  };
+  return operations;
 }
 
-
-
-
+const calculate = calculator();
+calculate.init("#num1", "#num2", "#result");
