e2ab875bbd43f53ddf08330bd3160eac0fa95a7d
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-04-22 18:51:37 +0300

    Soved via video

--- JSAdvanced/PreStudy/Unit Testing and Error Handling - Lab/02. Playing Cards.js
+++ JSAdvanced/PreStudy/Unit Testing and Error Handling - Lab/02. Playing Cards.js
@@ -1,30 +1,36 @@
 function solve(face, suit) {
-  class Card {
-    constructor(f, s) {
-      this.face = f;
-      this.suit = s;
-    }
+  let validSuits = { S: "\u2660", H: "\u2665", D: "\u2666", C: "\u2663" };
+  let validFaces = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
 
-    set suit(val) {
-      let valid = { S: "\u2660", H: "\u2665", D: "\u2666", C: "\u2663" };
-    }
+  face = face.toUpperCase();
+  suit = suit.toUpperCase();
 
-    set face(val) {
-      let valid = [
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10",
-        "J",
-        "Q",
-        "K",
-        "A",
-      ];
-    }
+  if (validFaces.includes(face) == false) {
+    throw new Error("Error invalid Suit");
+  } else if (Object.keys(validSuits).includes(suit) == false) {
+    throw new Error("Error invalid Face");
   }
+
+  return {
+    face,
+    suit,
+    toString() {
+      return `${face}${validSuits[suit]}`;
+    },
+  };
 }
+console.log(new solve("A", "S").toString());
