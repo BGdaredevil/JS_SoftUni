6867c8e95faa344dd1b6b8aba16f6915880f9ff8
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-05-14 23:04:23 +0300

    i have no idea

--- JSAdvanced/PreStudy/Classes and Attributes - Exercise/13. Payment Package.test.js
+++ JSAdvanced/PreStudy/Classes and Attributes - Exercise/13. Payment Package.test.js
@@ -72,22 +72,83 @@ class PaymentPackage {
 }
 
 describe("Class Payment Tests", function () {
-  let paymentInstance = new PaymentPackage("ivan", 10);
+  // it("initialization", function () {
+  //   expect(() => {
+  //     new PaymentPackage("pesho");
+  //   }).to.throw(Error);
+  //   expect(() => {
+  //     new PaymentPackage("pesho", {});
+  //   }).to.throw(Error);
+  //   expect(() => {
+  //     new PaymentPackage(10, 10);
+  //   }).to.throw(Error);
+  // });
   it("name has validation", function () {
-    expect(paymentInstance.name).to.equal("ivan");
+    let paymentInstance = new PaymentPackage("ivan", 10);
     expect(() => {
       paymentInstance.name = "";
     }).to.throw(Error);
+    expect(() => {
+      paymentInstance.name = {};
+    }).to.throw(Error);
+    expect(paymentInstance.name).to.equal("ivan");
+    paymentInstance.name = "pesho";
+    expect(paymentInstance.name).to.equal("pesho");
   });
   it("value has validation", function () {
-    expect(paymentInstance.value).to.equal(10);
+    let paymentInstance = new PaymentPackage("ivan", 10);
     expect(() => {
       paymentInstance.value = "";
     }).to.throw(Error);
-  });
-  it("has validation", function () {
     expect(() => {
       paymentInstance.value = -5;
     }).to.throw(Error);
+    expect(paymentInstance.value).to.equal(10);
+    paymentInstance.value = 5;
+    expect(paymentInstance.value).to.equal(5);
+  });
+  it("VAT has validation", function () {
+    let paymentInstance = new PaymentPackage("ivan", 10);
+    expect(paymentInstance.VAT).to.equal(20);
+    expect(() => {
+      paymentInstance.VAT = "";
+    }).to.throw(Error);
+    expect(() => {
+      paymentInstance.VAT = -5;
+    }).to.throw(Error);
+    expect(paymentInstance.VAT).to.equal(20);
+    paymentInstance.VAT = 10;
+    expect(paymentInstance.VAT).to.equal(10);
+  });
+  it("active has validation", function () {
+    let paymentInstance = new PaymentPackage("ivan", 10);
+    //expect(typeof paymentInstance.active).to.equal("boolean");
+    expect(paymentInstance.active).to.equal(true);
+    //expect(paymentInstance.active).to.equal(false);
+    // expect(() => {
+    //   paymentInstance.active = "";
+    // }).to.throw(Error);
+    expect(() => {
+      paymentInstance.active = {};
+    }).to.throw(Error);
+    expect(() => {
+      paymentInstance.active = "true";
+    }).to.throw(Error);
+  });
+  it("toString test", function () {
+    let paymentInstance = new PaymentPackage("ivan", 10);
+    expect(typeof paymentInstance.toString()).to.equal("string");
+    paymentInstance.active = false;
+    expect(
+      paymentInstance.toString().split(/\s/).includes("(inactive)")
+    ).to.equal(true);
+    paymentInstance.active = true;
+    expect(
+      paymentInstance.toString().split(/\s/).includes("(inactive)")
+    ).to.equal(false);
+    let temp = new PaymentPackage("HR Services", 1500);
+    expect(temp.toString()).to.equal(
+      "Package: HR Services\n- Value (excl. VAT): 1500\n- Value (VAT 20%): 1800"
+    );
   });
 });
