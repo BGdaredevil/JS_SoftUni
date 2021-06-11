767d59201876c3cfd900a849981df17532e293db
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-06-11 22:53:43 +0300

    FINALLY solved

--- JSAdvanced/PreStudy/Prototypes and Inheritance - Exercise/02. String Extension.js
+++ JSAdvanced/PreStudy/Prototypes and Inheritance - Exercise/02. String Extension.js
@@ -1,27 +1,51 @@
 (function () {
   String.prototype.ensureStart = function (str) {
-    return this.startsWith(str) ? this.toString() : str + this;
+    return this.toString().startsWith(str)
+      ? this.toString()
+      : str + this.toString();
   };
   String.prototype.ensureEnd = function (str) {
-    return this.endsWith(str) ? this.toString() : this + str;
+    return this.toString().endsWith(str)
+      ? this.toString()
+      : this.toString() + str;
   };
   String.prototype.isEmpty = function () {
-    return this.length === 0;
+    return this.length === 0 ? true : false;
   };
   String.prototype.truncate = function (n) {
-    if (n < 4) {
+    let str = this.toString();
+
+    if (str.length < 4) {
       return ".".repeat(n);
     }
-    if (this.length < n) {
-      return this.toString();
-    } else if (this.includes(" ")) {
-      let val = this.slice(0, n - 2);
-      return val.slice(0, val.lastIndexOf(" ")) + "...";
-    } else {
-      return this.slice(0, n - 2) + "...";
+    if (str.length <= n) {
+      return str;
     }
+
+    if (str.length > n) {
+      str = str.substring(0, n);
+    }
+
+    str = str.substring(0, n - 2);
+
+    if (!str.includes(" ")) {
+      return str + "...";
+    } else {
+      return str.substring(0, str.lastIndexOf(" ")) + "...";
+    }
+
+    // let info = this.slice(0, n);
+    // if (!info.includes(" ")) {
+    //   return info.slice(0, n - 3) + "...";
+    // }
+    // let val = info.slice(0, n);
+    // let loc = val.lastIndexOf(" ");
+    // return loc === -1
+    //   ? info.slice(0, n - 3) + "..."
+    //   : val.slice(0, val.lastIndexOf(" ")) + "...";
   };
   String.format = function (str, ...params) {
+    str = str.toString();
     params.map((el, loc) => {
       loc = "{" + loc;
       let reg = new RegExp(loc);
@@ -34,9 +58,17 @@
   };
 })();
 
-var testString = "the {0} brown {1} jumps over the {2} dog";
-
-console.log(String.format(testString, "quick", "fox", "lazy", "bananas")); //.to.equal(
-//   "the quick brown fox jumps over the lazy dog",
-//   "Incorrect format() functionality"
-// );
+//onsole.log(String.hasownProperty("format"));
+let str = "1234567890";
+// str = str.ensureStart("hello ");
+// console.log(str);
+str = str.truncate(11);
+console.log(str);
+str = str.truncate(10);
+console.log(str);
+str = str.truncate(9);
+console.log(str);
+str = str.truncate(8);
+console.log(str);
+str = str.truncate(7);
+console.log(str);
