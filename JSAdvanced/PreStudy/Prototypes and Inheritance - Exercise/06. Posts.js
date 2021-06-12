dd80129e60c79c86eca02f4e4dd447a4b7636ff5
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-06-12 13:09:47 +0300

    i have no idea why this does not work

---
+++ JSAdvanced/PreStudy/Prototypes and Inheritance - Exercise/06. Posts.js
@@ -0,0 +1,80 @@
+function solve() {
+  class Post {
+    constructor(...input) {
+      let index = 0;
+      this.title = input[index++];
+      this.content = input[index++];
+    }
+    toString() {
+      return [`Post: ${this.title}`, `Content: ${this.content}`].join("\n");
+    }
+  }
+  class SocialMediaPost extends Post {
+    constructor(...input) {
+      let index = 0;
+      super(input[index++], input[index++]);
+      this.likes = input[index++];
+      this.dislikes = input[index++];
+      this.comments = [];
+    }
+    addComment(str) {
+      this.comments.push(str);
+    }
+    toString() {
+      let temp = [super.toString()];
+      temp.push(`Rating: ${this.likes - this.dislikes}`);
+      if (this.comments.length > 0) {
+        temp.push(
+          ...this.comments.reduce((a, e) => (a.push(` * ${e})`), a), [
+            "Comments:",
+          ])
+        );
+      }
+      return temp.join("\n");
+    }
+  }
+  class BlogPost extends Post {
+    constructor(...input) {
+      let index = 0;
+      super(input[index++], input[index++]);
+      this.views = input[index++];
+    }
+    view() {
+      this.views++;
+      return this;
+    }
+    toString() {
+      let temp = [super.toString()];
+      temp.push(`Views: ${this.views}`);
+      return temp.join("\n");
+    }
+  }
+  return { Post, SocialMediaPost, BlogPost };
+}
+
+const classes = solve();
+// let post = new classes.Post("Post", "Content");
+
+// console.log(post.toString());
+
+// // Post: Post
+// // Content: Content
+
+// let scm = new classes.SocialMediaPost("TestTitle", "TestContent", 25, 30);
+
+// scm.addComment("Good post");
+// scm.addComment("Very good post");
+// scm.addComment("Wow!");
+
+// console.log(scm.toString());
+
+// // Post: TestTitle
+// // Content: TestContent
+// // Rating: -5
+// // Comments:
+// //  * Good post
+// //  * Very good post
+// //  * Wow!
+let item = new classes.BlogPost("TestTitle", "TestContent", 5);
+item.view().view().view();
+console.log(item.toString());
