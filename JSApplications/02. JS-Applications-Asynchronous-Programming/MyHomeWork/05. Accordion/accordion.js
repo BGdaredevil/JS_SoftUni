2ac04eb857d165ba15912534c42c8f8925cbd6ae
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-07-06 23:14:09 +0300

    Solved- Funny behaviour - order depends on throtle

--- JSApplications/02. JS-Applications-Asynchronous-Programming/MyHomeWork/05. Accordion/accordion.js
+++ JSApplications/02. JS-Applications-Asynchronous-Programming/MyHomeWork/05. Accordion/accordion.js
@@ -1,3 +1,81 @@
-function solution() {
-    //TODO .....
-}
+(function solution() {
+  let mainWorkSpace = document.querySelector("#main");
+  mainWorkSpace.innerHTML = "";
+
+  fetch(`http://localhost:3030/jsonstore/advanced/articles/list`)
+    .then((r) => r.json())
+    .then((res) => {
+      res.map(({ _id, title }) => {
+        let baseUrl =
+          "http://localhost:3030/jsonstore/advanced/articles/details/";
+        fetch(`${baseUrl}${_id}`)
+          .then((r) => r.json())
+          .then((info) => {
+            mainWorkSpace.appendChild(
+              e(
+                "div",
+                { classes: ["accordion"] },
+                e(
+                  "div",
+                  { classes: ["head"] },
+                  e("span", {}, title),
+                  e(
+                    "button",
+                    {
+                      classes: ["button"],
+                      attributes: { id: _id },
+                      listener: { click: moreBtn },
+                    },
+                    "More"
+                  )
+                ),
+                e("div", { classes: ["extra"] }, e("p", {}, info.content))
+              )
+            );
+          });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  function moreBtn(ev) {
+    ev.target.innerText = ev.target.innerText === "MORE" ? "LESS" : "MORE";
+    ev.target.parentElement.nextSibling.style.display =
+      ev.target.parentElement.nextSibling.style.display === "block"
+        ? "none"
+        : "block";
+  }
+
+  function e(tag, obj, ...content) {
+    let result = document.createElement(tag);
+
+    if (obj.hasOwnProperty("listener")) {
+      Object.entries(obj.listener).forEach(([ev, han]) => {
+        result.addEventListener(ev, han);
+      });
+    }
+
+    if (obj.hasOwnProperty("attributes")) {
+      Object.entries(obj.attributes).forEach(([attr, val]) => {
+        result[attr] = val;
+      });
+    }
+
+    if (obj.hasOwnProperty("classes")) {
+      obj.classes.forEach((c) => {
+        result.classList.add(c);
+      });
+    }
+
+    content.forEach((c) => {
+      if (typeof c === "number" || typeof c === "string") {
+        result.innerText = c;
+      } else {
+        result.appendChild(c);
+      }
+    });
+
+    return result;
+  }
+})();
