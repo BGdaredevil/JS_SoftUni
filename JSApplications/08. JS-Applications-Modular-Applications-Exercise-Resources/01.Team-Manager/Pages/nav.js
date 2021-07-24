2cc08b2d47152ff99c6babadd3ced6c079f17edd
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-07-25 00:11:29 +0300

    s0lved

--- JSApplications/08. JS-Applications-Modular-Applications-Exercise-Resources/01.Team-Manager/Pages/nav.js
+++ JSApplications/08. JS-Applications-Modular-Applications-Exercise-Resources/01.Team-Manager/Pages/nav.js
@@ -5,17 +5,28 @@ const settings = {
   location,
 };
 
-const navTemplate = (isUser) => html`<a href="/home" class="site-logo">Team Manager</a>
+const navTemplate = (isUser, currLoc) => html`<a href="/home" class="site-logo">Team Manager</a>
   <nav>
-    <a href="/browse" class="action">Browse Teams</a>
-    ${isUser ? "" : html`<a href="/login" class="action">Login</a>`}
-    ${isUser ? "" : html`<a href="/register" class="action">Register</a>`}
-    ${isUser ? html`<a href="/my-teams" class="action">My Teams</a>` : ""}
+    <a href="/browse" class="action ${currLoc === "browse" ? "active" : ""}">Browse Teams</a>
+    ${isUser
+      ? ""
+      : html`<a href="/login" class="action ${currLoc === "login" ? "active" : ""}">Login</a>`}
+    ${isUser
+      ? ""
+      : html`<a href="/register" class="action ${currLoc === "register" ? "active" : ""}"
+          >Register</a
+        >`}
+    ${isUser
+      ? html`<a href="/my-teams" class="action ${currLoc === "my-teams" ? "active" : ""}"
+          >My Teams</a
+        >`
+      : ""}
     ${isUser ? html`<a href="/logout" class="action">Logout</a>` : ""}
   </nav>`;
 
 async function getView(ctx, next) {
-  ctx.navRender(navTemplate(isLogged()));
+  let currLoc = ctx.path.split("/").filter((x) => x.length > 0)[0];
+  ctx.navRender(navTemplate(isLogged(), currLoc));
   next();
 }
 
